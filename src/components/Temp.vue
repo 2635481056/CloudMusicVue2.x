339d01fe2038710dbe775381be65275f38caca51
2635481056 <2635481056@qq.com>
2022-02-26 01:21:57 +0800

    ddddd

---
+++ src/components/Temp.vue
@@ -0,0 +1,34 @@
+<template>
+	<div id=""  >
+		<ul>
+			<li v-for="m,index in msg" :key="index">{{index}} {{m.songName}}</li>
+	
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'Temp',
+		props:{
+			msg:{
+				type:Array,
+				default(){
+					return [
+					{songName:"name1" , singerName:"name1"},
+					{songName:"name2" , singerName:"name3"},
+					{songName:"name3" , singerName:"name4"},
+					{songName:"name4" , singerName:"name5"},
+				]}
+			}
+		}
+	}
+	
+</script>
+
+<style>
+	*{
+		color: white !important;
+	}
+	
+</style>
